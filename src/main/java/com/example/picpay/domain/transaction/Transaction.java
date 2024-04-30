71854fb278ae2e91b0fcd498d7a5c0dbfb205238
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-04-30 08:58:56 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/example/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,31 @@
+package com.example.picpay.domain.transaction;
+
+
+import com.example.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal ammount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
